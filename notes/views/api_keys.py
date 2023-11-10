22a4109927aade466eaed98e12ff70a5c9c6ee56
nicknameisavailable <iamgenii@yandex.ru>
2023-11-10 12:45:13 +0300

    валидация ключа опенаи

--- notes/views/api_keys.py
+++ notes/views/api_keys.py
@@ -1,15 +1,40 @@
-import os
-
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import AccessToken
+from openai import OpenAI
+from notes.models import User
 from rest_framework.response import Response
 from rest_framework import status
 
 
 class ApiKeys(APIView):
     def post(self, request, *args, **kwargs):
+        token = AccessToken(request.headers["Authorization"].replace("Bearer ", ""))
+        user_id = token.payload["user_id"]
         key = request.data["key"]
-        # response = save_key(key)
-        # return Response(response, status=status.HTTP_200_OK, content_type="application/json")
+        client = OpenAI()
+
+        try:
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "user",
+                        "content": ""
+                    }
+                ],
+                temperature=1,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+        except Exception as e:
+
+
+        user = User.objects.get(id=user_id).to_json()
+
+        response = {"success": True, "message": "Ключ OpenAI сохранен", "key": key}
+        return Response(response, status=status.HTTP_200_OK, content_type="application/json")
 
     def get(self, request, *args, **kwargs):
         key_id = request.query_params.get("id")
